de7d2c37a39c7448ad81ce2ac30153ca07c31a9b
Marc S. Thomson <mthomson858@gmail.com>
2018-09-01 17:08:11 -0400

    updated for chapter 7

--- lists/tests.py
+++ lists/tests.py
@@ -18,7 +18,7 @@ class HomePageTest(TestCase):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
 
 	def test_only_saves_items_when_necessary(self):
 		self.client.get('/')
@@ -51,4 +51,15 @@ class ItemModelTest(TestCase):
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'The second list item')
+
+class ListViewTest(TestCase):
+
+	def test_displays_all_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/lists/the-only-list-in-the-world')
+
+		self.assertContains(response, 'itemey 1', status_code=301)
+		self.assertContains(response, 'itemey 2', status_code=301)
 
