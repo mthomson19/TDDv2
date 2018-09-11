e7984fec931ded251f41193e50905feecfc10d1b
Marc S. Thomson <mthomson858@gmail.com>
2018-09-11 15:01:32 -0400

    end of chapter 14

--- lists/forms.py
+++ lists/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
@@ -17,3 +18,9 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
--- lists/views.py
+++ lists/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import redirect, render
-from django.core.exceptions import ValidationError
-from lists.forms import ItemForm
-from lists.models import Item, List
 
-# Create your views here.
+from lists.forms import ItemForm
+from lists.models import List
+
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ItemForm()
-
-    if request.method == 'POST':
-        form = ItemForm(data=request.POST)
-        if form.is_valid():
-            item = Item.objects.create(text=request.POST['text'], list=list_)
-            return redirect(list_)
-    return render(request, 'list.html', {'list': list_, "form": form})
 
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        item = Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    form = ItemForm()
+    if request.method == 'POST':
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            form.save(for_list=list_)
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
+
