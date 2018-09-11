29a4b36e371ab87e88d7c13225e9edd804dd56c9
Marc S. Thomson <mthomson858@gmail.com>
2018-09-11 17:14:58 -0400

    Implement duplicate item validation at model layer

--- lists/models.py
+++ lists/models.py
@@ -9,9 +9,9 @@ class List(models.Model):
         return reverse('view_list', args=[self.id])
 
 
-
-
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
 
+    class Meta:
+        unique_together = ('list', 'text')
